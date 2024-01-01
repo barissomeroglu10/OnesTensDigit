0a66b771fa44e93b2e18f44117f1a8377eb141d5
baris <baris@BARIS_SOMEROGLU>
2024-01-01 15:27:22 +0300

    Add project files.

---
+++ OnesTensDigit/OnesTensDigit.cpp
@@ -0,0 +1,35 @@
+﻿/*
+In this code, we will get two digits number from user. And write tens digit and ones digit for the number.
+
+Developer: Barış Someroğlı
+Date: 01.01.24 / 3:20 p.m.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Number, Ones, Tens;
+
+	cout << "Please Enter 'two digits' Number: ";
+	cin >> Number;
+
+	if ((Number / 10) >= 10)
+	{
+		cout << "You Should Enter Two Digits Number!" << endl;
+	}
+
+	else
+	{
+		Ones = Number % 10;
+		Number = Number / 10;
+		Tens = Number;
+
+		cout << "Ones Digit: " << Ones << endl;
+		cout << "Tens Digit: " << Tens << endl;
+	}
+
+	return 0;
+}
